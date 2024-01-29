58e2981b9d186e53d8628d7c3eaf65eb10e6b20c
wpmohammad1 <alimohammadjhd@gmail.com>
2024-01-29 14:30:29 +0600

    added banner

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import Container from '../../../Shared/Container/Container';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './Banner.css'
+import 'swiper/css';
+import 'swiper/css/effect-fade';
 
+
+import { Autoplay, EffectFade } from 'swiper/modules';
 const Banner = () => {
     return (
         <div>
             <Container>
-                            Banner
-
+                <>
+                    <Swiper
+                        effect={'fade'}
+                        navigation={true}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        modules={[EffectFade, Autoplay]}
+                        className="mySwiper"
+                    >
+                            <SwiperSlide>
+                            <img className='sm:h-[450px] h-64 w-full' src="https://i.ibb.co/dmy1vNH/events.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <img  className='sm:h-[450px] h-64 w-full' src="https://i.ibb.co/pPnLStt/laptop.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <img  className='sm:h-[450px] h-64 w-full' src="https://i.ibb.co/SNRZG1D/offer.jpg" />
+                        </SwiperSlide>
+                    </Swiper>
+                </>
             </Container>
         </div>
     );
--- src/Pages/Shared/Container/Container.jsx
+++ src/Pages/Shared/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className="dark:bg-gray-800 dark:text-white max-w-[2520px] mx-auto xl:px-32 md:px-20 sm:px-2 px-4">
+    <div className="dark:bg-gray-800 dark:text-white max-w-[2520px] mx-auto xl:px-24 md:px-20 sm:px-5 px-4">
       {children}
     </div>
   );
